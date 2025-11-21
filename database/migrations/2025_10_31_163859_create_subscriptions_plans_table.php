fa1c722a1019729ee07d2af4602eba2eedf2836b
Muhammad Usman <muhammadusmandev@yahoo.com>
2025-11-21 09:17:45 -0800

    minor fixes and changes

--- database/migrations/2025_10_31_163859_create_subscriptions_plans_table.php
+++ database/migrations/2025_10_31_163859_create_subscriptions_plans_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('subscription_plans');
     }
 };
