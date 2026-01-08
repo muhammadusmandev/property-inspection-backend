04d6a9881e55da1b9067f98ebdd48119b3f2abd0
Muhammad Usman <muhammadusmandev@yahoo.com>
2026-01-08 09:26:18 -0500

    Temp remove rate limit middlewares

--- routes/api.php
+++ routes/api.php
@@ -26,19 +26,19 @@ use App\Http\Controllers\Api\Country\CountryController;
 Route::prefix('v1')->group(function () {
 
     Route::prefix('auth')->group(function () {
-        Route::post('login', [AuthController::class, 'login'])->middleware('throttle:login');
-        Route::post('register', [AuthController::class, 'register'])->middleware('throttle:register');
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
         Route::delete('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     });
 
     Route::prefix('otp')->group(function () {
-        Route::post('verify', [OtpVerificationController::class, 'verify'])->middleware('throttle:verify-otp');
-        Route::post('resend', [OtpVerificationController::class, 'resend'])->middleware('throttle:send-otp');
+        Route::post('verify', [OtpVerificationController::class, 'verify']);
+        Route::post('resend', [OtpVerificationController::class, 'resend']);
     });
 
     Route::prefix('forgot-password')->group(function () {
-        Route::post('/request', [ForgotPasswordController::class, 'request'])->middleware('throttle:request-otp');
-        Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword'])->middleware('throttle:reset-password');
+        Route::post('/request', [ForgotPasswordController::class, 'request']);
+        Route::post('/reset-password', [ForgotPasswordController::class, 'resetPassword']);
     });
 
     Route::get('countries/list', [CountryController::class, 'index']);
